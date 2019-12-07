9bc4d07017fdafd49e969528d7febd93f4eadc53
Duc <duc.h@hotmail.com>
2019-12-07 20:51:21 +0900

    moved to spring and runs

--- src/main/java/com/ledger/backend/LedgerAppApplication.java
+++ src/main/java/com/ledger/backend/LedgerAppApplication.java
@@ -1,7 +1,10 @@
 package com.ledger.backend;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LedgerAppApplication {
@@ -10,4 +13,18 @@ public class LedgerAppApplication {
 		SpringApplication.run(LedgerAppApplication.class, args);
 	}
 
+	@Bean
+	public void run() {
+		Main main = new Main();		
+
+		System.out.println("Enter mode: (1 or 2)");
+		Scanner sc = new Scanner(System.in);
+		String mode = sc.nextLine();
+		if(mode.equals("1")) {
+			main.menu();
+		}else {
+			main.maidSplit();
+		}
+		main.start();
+	}
 }
